051d7918d2b964f1e897bde9df188e527883264e
Abdulrhman-Khaled <bodyono3@gmail.com>
2025-10-15 12:33:49 +0000

    add sliders and categories

---
+++ salon/salon/doctype/categories/categories.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, ITQAN and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+class Categories(NestedSet):
+	pass
