9ff681ab64e0fb63fb81617f2ea09838953b252a
Abdulrhman-Khaled <bodyono3@gmail.com>
2025-10-23 16:51:35 +0300

    add lat lng button

--- salon/salon/doctype/booking/booking.js
+++ salon/salon/doctype/booking/booking.js
@@ -1,21 +1,48 @@
 frappe.ui.form.on('Booking', {
-    calculate_total: function(frm) {
+    refresh: function (frm) {
+        if (frm.doc.lat_lng) {
+            frm.add_custom_button(__('Open Location in Maps'), function () {
+                let latLng = frm.doc.lat_lng;
+
+                if (!latLng || !latLng.includes('-')) {
+                    frappe.msgprint(__('Invalid location format.'));
+                    return;
+                }
+
+                let [lat, lng] = latLng.split('-');
+
+                if (!lat || !lng) {
+                    frappe.msgprint(__('Invalid location data.'));
+                    return;
+                }
+
+                let mapsUrl = `https://www.google.com/maps?q=${lat},${lng}`;
+                window.open(mapsUrl, '_blank');
+            }).addClass('btn-primary');
+        } else {
+            frm.add_custom_button(__('Open Location in Maps'), function () {
+                frappe.msgprint(__('No location found for this booking.'));
+            });
+        }
+    },
+
+    calculate_total: function (frm) {
         let total = 0;
         (frm.doc.table_services || []).forEach(row => {
             total += row.total_price || 0;
         });
         frm.set_value('total', total);
     },
-    table_services_add: function(frm) {
+    table_services_add: function (frm) {
         frm.trigger('calculate_total');
     },
-    table_services_remove: function(frm) {
+    table_services_remove: function (frm) {
         frm.trigger('calculate_total');
     }
 });
 
 frappe.ui.form.on('Booking Items List', {
-    service: function(frm, cdt, cdn) {
+    service: function (frm, cdt, cdn) {
         const row = locals[cdt][cdn];
         if (row.service) {
             frappe.db.get_value('Service', row.service, 'price').then(r => {
@@ -27,12 +54,12 @@ frappe.ui.form.on('Booking Items List', {
             });
         }
     },
-    qty: function(frm, cdt, cdn) {
+    qty: function (frm, cdt, cdn) {
         const row = locals[cdt][cdn];
         frappe.model.set_value(cdt, cdn, 'total_price', (row.qty || 0) * (row.price || 0));
         frm.trigger('calculate_total');
     },
-    price: function(frm, cdt, cdn) {
+    price: function (frm, cdt, cdn) {
         const row = locals[cdt][cdn];
         frappe.model.set_value(cdt, cdn, 'total_price', (row.qty || 0) * (row.price || 0));
         frm.trigger('calculate_total');
